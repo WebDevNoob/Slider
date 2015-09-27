25122a11644a3ad4ca5b609543272a8614c16246
WebDevNoob <johngpetersen@yahoo.com>
2015-09-26 23:04:19 -0400

    Updated comments line by line

--- js/slider - Commented.js
+++ js/slider - Commented.js
@@ -1,19 +1,15 @@
-//When document is loaded, fade in the first element in the #slider > img div,
-//then start looping through images. 
+/*When document is loaded, fade in the first element in the #slider > img div,
+then start looping through images. Sets the global sliderNext var, which is used to find current slide for functions*/
+var sliderNext = 0;
 $(document).ready(function () {
-//    'use strict';
     $("#slider > img").first().fadeIn(300);
     startSlider();
 });
 //Modular SlideShow Starters
 function startSlider() {
-//    'use strict';
-    var sliderNext, loop;
-//pre-set sliderNext to 0
-    sliderNext = 0;
-//loop is not nessasary to set yet, setInterval is the main looping function
+//Main Loop runs every 3 seconds
     loop = setInterval(function () {
- //add one to slidernext to move it to the next slide   
+//Add one to slidernext to move it to the next slide  
         sliderNext = sliderNext + 1;
 //if sliderNext is greater then the length of of the div minus one set sliderNext back to 0
         if (sliderNext > (($("#slider > img").size() - 1))) {
@@ -23,8 +19,46 @@ function startSlider() {
         $("#slider > img").fadeOut(300);
 //Fade in new div img using concat + sliderNext
         $("#slider > img#" + sliderNext).fadeIn(300);
-//Wait 3 seconds in between each iteration
+//Wait 3 seconds in between iterations
     }, 3000);
 }
+
+//Previous Button Function - showSlide func uses sliderNext global var minus one to go back a slide
+function prev(){
+    showSlide((sliderNext - 1));
+};  
+//Next Button Function - showSlide func uses sliderNext global var pus one to go foward a slide 
+function next(){
+    showSlide((sliderNext + 1));
+};
+//Function to show a called slide arg is placeholder var thats assigned by the prev() or next()
+function showSlide(arg){
+//Clears the Interval of time so it doesn't bunch up on itself
+    window.clearInterval(loop);
+//Checking bounds
+        if (arg > (($("#slider > img").size() - 1))) {
+            arg = 0;
+        }else if (arg < 1) {
+            arg = (($("#slider > img").size() - 1));
+        }
+//Fade out previous div img
+        $("#slider > img").fadeOut(300);
+//Fade in new div img using concat + sliderNext    
+        $("#slider > img#" + arg).fadeIn(300);
+//Updates sliderNext to placeholder var to restart interval
+    sliderNext = arg;
+//Restarts Slider    
+    startSlider();
+}   
+
+//When the slider is hovered over, clears the interval, when removed, starts slider again
+$("#slider > img").hover(
+    function(){
+        window.clearInterval(loop);
+    },
+    function(){
+        startSlider();
+    });
+
 
 
