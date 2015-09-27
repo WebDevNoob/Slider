f61c4f0af3c7b8b09d26059fe712b5a8a8cc77ce
WebDevNoob <johngpetersen@yahoo.com>
2015-09-27 12:28:29 -0400

    Updated and changed for better readability

--- js/slider - Commented.js
+++ js/slider - Commented.js
@@ -1,64 +1,54 @@
 /*When document is loaded, fade in the first element in the #slider > img div,
 then start looping through images. Sets the global sliderNext var, which is used to find current slide for functions*/
-var sliderNext = 0;
+var sliderNext = 1;
+var loop;
 $(document).ready(function () {
     $("#slider > img").first().fadeIn(300);
-    startSlider();
+    showSlide();
 });
-//Modular SlideShow Starters
-function startSlider() {
-//Main Loop runs every 3 seconds
-    loop = setInterval(function () {
-//Add one to slidernext to move it to the next slide  
-        sliderNext = sliderNext + 1;
-//if sliderNext is greater then the length of of the div minus one set sliderNext back to 0
+//Previous Button Function - showSlide func uses sliderNext global var minus one to go back a slide
+function prev(){
+    sliderNext = sliderNext - 2;
+    showSlide();
+};
+//Next Button Function - showSlide func uses sliderNext global var pus one to go foward a slide 
+function next(){
+    sliderNext = sliderNext;
+    showSlide();
+};
+
+function showSlide(){
+//If currently in loop clear interval, check if in bounds, then display picture. 
+    if(loop){
+        window.clearInterval(loop);
         if (sliderNext > (($("#slider > img").size() - 1))) {
             sliderNext = 0;
+        }else if (sliderNext < 0) {
+            sliderNext = (($("#slider > img").size() - 1));
         }
-//Fade out previous div img
         $("#slider > img").fadeOut(300);
-//Fade in new div img using concat + sliderNext
+        $("#slider > img#" + sliderNext).fadeIn(300);   
+    sliderNext++;
+    }    
+//If not currently in loop, check bounds, then display picture and cycle pictures every 3 seconds
+    loop = setInterval(function () {
+        if (sliderNext > (($("#slider > img").size() - 1))) {
+            sliderNext = 0;
+        }else if (sliderNext < 1) {
+            sliderNext = (($("#slider > img").size() - 1));
+        }
+        $("#slider > img").fadeOut(300);
         $("#slider > img#" + sliderNext).fadeIn(300);
-//Wait 3 seconds in between iterations
+    sliderNext++;
     }, 3000);
 }
 
-//Previous Button Function - showSlide func uses sliderNext global var minus one to go back a slide
-function prev(){
-    showSlide((sliderNext - 1));
-};  
-//Next Button Function - showSlide func uses sliderNext global var pus one to go foward a slide 
-function next(){
-    showSlide((sliderNext + 1));
-};
-//Function to show a called slide arg is placeholder var thats assigned by the prev() or next()
-function showSlide(arg){
-//Clears the Interval of time so it doesn't bunch up on itself
-    window.clearInterval(loop);
-//Checking bounds
-        if (arg > (($("#slider > img").size() - 1))) {
-            arg = 0;
-        }else if (arg < 1) {
-            arg = (($("#slider > img").size() - 1));
-        }
-//Fade out previous div img
-        $("#slider > img").fadeOut(300);
-//Fade in new div img using concat + sliderNext    
-        $("#slider > img#" + arg).fadeIn(300);
-//Updates sliderNext to placeholder var to restart interval
-    sliderNext = arg;
-//Restarts Slider    
-    startSlider();
-}   
-
 //When the slider is hovered over, clears the interval, when removed, starts slider again
-$("#slider > img").hover(
-    function(){
+$("#slider > img").hover(function(){
         window.clearInterval(loop);
     },
-    function(){
-        startSlider();
-    });
-
+        function(){
+        setTimeout(showSlide(),3000);
+});
 
 
